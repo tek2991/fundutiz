c729939f0d8d8158c03004f3fb08ebea06729d8b
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-04-11 11:55:07 +0530

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,6 @@ Route::group(['middleware' => ['auth:sanctum', config('jetstream.auth_session'),
 
     Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['is_admin']], function () {
         Route::resource('financial_year', FinancialYearController::class)->only('index', 'create', 'store', 'edit', 'update');
-        Route::resource('fund', FundController::class);
+        Route::resource('fund', FundController::class)->only('index', 'create', 'store', 'edit', 'update', 'destroy');
     });
 });
