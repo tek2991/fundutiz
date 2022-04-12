d988a39de3f1be2135c3ef31f75f977ee73a2b7f
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-04-12 16:37:42 +0530

    Added sactioner and transaction migrations

---
+++ database/migrations/2022_04_12_102914_create_transactions_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('fund_id')->constrained();
+            $table->foreignId('team_id')->constrained();
+            $table->string('type');
+            $table->string('status')->nullable();
+            $table->date('sactioned_at');
+            $table->bigInteger('amount_in_cents');
+            $table->string('item')->nullable();
+            $table->string('vendor_name')->nullable();
+            $table->string('file_number')->nullable();
+            $table->boolean('is_gem')->nullable();
+            $table->string('non_gem_remark')->nullable();
+            $table->boolean('gem_non_availability')->nullable();
+            $table->string('gem_non_availability_remark')->nullable();
+            $table->foreignId('creator_user_id')->constrained('users');
+            $table->foreignId('updator_user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
