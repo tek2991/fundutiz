d85fa92e7a3f4a6a6fa2099f31727824cfeed592
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-06-07 16:33:51 +0530

    Added PendingTeamInvitationForm

---
+++ resources/js/Pages/Profile/Partials/PendingTeamInvitationForm.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+</script>
+
+<template>
+    <div
+        v-if="
+            team.team_invitations.length > 0 &&
+            userPermissions.canAddTeamMembers
+        ">
+        <JetSectionBorder />
+
+        <!-- Team Member Invitations -->
+        <JetActionSection class="mt-10 sm:mt-0">
+            <template #title> Pending Team Invitations </template>
+
+            <template #description>
+                You have been invited to join these teams and have been sent
+                an invitation email. They may join the team by accepting the
+                invitation.
+            </template>
+
+            <!-- Pending Team Member Invitation List -->
+            <template #content>
+                <div class="space-y-6">
+                    <div
+                        v-for="invitation in team.team_invitations"
+                        :key="invitation.id"
+                        class="flex items-center justify-between">
+                        <div class="text-gray-600">
+                            {{ invitation.email }}
+                        </div>
+
+                        <div class="flex items-center">
+                            <!-- Cancel Team Invitation -->
+                            <button
+                                v-if="userPermissions.canRemoveTeamMembers"
+                                class="cursor-pointer ml-6 text-sm text-red-500 focus:outline-none"
+                                @click="cancelTeamInvitation(invitation)">
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </template>
+        </JetActionSection>
+    </div>
+</template>
